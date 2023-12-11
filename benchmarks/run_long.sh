a81e64a639bbf6424ed0e7e9d14985fa6679f69a
Oh Jun <ohjun@umich.edu>
2023-12-11 17:46:18 -0500

    new script for long

--- benchmarks/run_long.sh
+++ benchmarks/run_long.sh
@@ -20,24 +20,12 @@ opt -load-pass-plugin="${PATH2LIB}" -passes="${PASS}" ${1}.bc -o ${1}.swpf.bc > 
 
 # Generate binary executable before swpf
 clang ${1}.bc -o ${1}_swpf_not
-# Produce output from binary to check correctness
-./${1}_swpf_not > swpf_not_output
 
 # Generate binary executable after swpf
 clang ${1}.swpf.bc -o ${1}_swpf
-# Produce output from binary to check correctness
-./${1}_swpf > swpf_output
-
-# # Convert source code to il
-# clang -emit-llvm -S ${1}.c -Xclang -disable-O0-optnone -o ${1}.ll
-# # Run pass on il
-# opt -load-pass-plugin="${PATH2LIB}" -passes="${PASS}" ${1}.ll -o ${1}.swpf.ll > /dev/null
 
 # Creates viz pdf
 sh viz.sh ${1}
-
-# check outputs match
-diff swpf_not_output swpf_output > ${1}.diff
 
 # Cleanup: Remove intermediate files
 rm -f *.ll *.bc
