c8c6fcf15649c89b399e55a9573e3d7195164cd5
Oh Jun <ohjun@umich.edu>
2023-12-12 12:26:29 -0500

    float benchmarks, final

---
+++ benchmarks/indir_double_big_long_float.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int x = 0;
+    int A[1000000];
+    int B[1000000];
+
+    // populate A
+    for (int i = 0; i < 1000000; ++i) {
+        A[i] = i;
+    }
+    // A = {999999, 1, 999997, 3, 999995, 5, ...}
+    for (int i = 0; i < 500000; ++i) {
+        A[i*2] = 999999-i*2;
+    }
+
+    // populate B
+    for (int i = 0; i < 1000000; ++i) {
+        B[i] = i;
+    }
+
+    for (int y = 0; y < 10000; ++y) {
+        // double indirection
+        for (int i = 0; i < 1000000; ++i) {
+            x += B[A[i]];
+            
+            // some overlapping work to exploit prefetching
+            float y = (float) x;
+            float z = y * x;
+            y = z / x;
+            z *= y;
+        }
+    }
+}
---
+++ benchmarks/indir_triple_big_long_float.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int SIZE = 600000;
+    int x = 0;
+    int A[600000];
+    int B[600000];
+    int C[600000];
+
+    // populate A
+    for (int i = 0; i < SIZE; ++i) {
+        A[i] = i;
+    }
+    // A = {999999, 1, 999997, 3, 999995, 5, ...}
+    for (int i = 0; i < SIZE/2; ++i) {
+        A[i*2] = (SIZE-1)-i*2;
+    }
+
+    // populate B
+    for (int i = 0; i < SIZE; ++i) {
+        B[i] = i;
+    }
+
+    // populate C
+    for (int i = 0; i < SIZE; ++i) {
+        C[i] = i;
+    }
+    // C = {0, 999999, 2, 999997, 4, 999995, ...}
+    for (int i = 0; i < SIZE/2; ++i) {
+        C[i*2 + 1] = (SIZE-1)-i*2;
+    }
+
+    for (int y = 0; y < 100000; ++y) {
+        // triple indirection
+        for (int i = 0; i < SIZE/2; ++i) {
+            x = C[B[A[i]]];
+
+            // some overlapping work to exploit prefetching
+            float y = (float) x;
+            float z = y * x;
+            y = z / x;
+            z *= y;
+        }
+    }
+}
